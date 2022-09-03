8b8f12bc76cd632bc4c9b0a50e9b505624821205
emphyrea <111723204+emphyrea@users.noreply.github.com>
2022-09-03 15:58:43 -0500

    Add files via upload

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿// using System;
+
+namespace Assignment2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string StudentName;
+            int StudentId;
+            string StudentMajor;
+            string StudentSemester; 
+
+            Console.Write("Enter your Name." + "\n");
+            StudentName = Console.ReadLine();
+
+            Console.Write("Enter your Student ID." + "\n");
+            StudentId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your Major." + "\n");
+            StudentMajor = Console.ReadLine();
+
+            Console.Write("Enter your current Semester." + "\n");
+            StudentSemester = Console.ReadLine();
+
+            Console.WriteLine(StudentName + " " + StudentId + " " + StudentSemester + " is taking " + StudentMajor + ".");
+
+        }
+    }
+}
